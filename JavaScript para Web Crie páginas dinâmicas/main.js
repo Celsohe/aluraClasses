bcf7d066b112debf81c354416ae013ec176de61d
Celsohe <30702924+Celsohe@users.noreply.github.com>
2023-02-10 09:42:32 -0300

    Teclado Organizado.

--- JavaScript para Web Crie páginas dinâmicas/main.js
+++ JavaScript para Web Crie páginas dinâmicas/main.js
@@ -1,3 +1,4 @@
+//Seletor de Música
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
@@ -10,15 +11,19 @@ function tocaSom (seletorAudio) {
     }
 }
 
+
+
+//Variáveis
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
+
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`; //template string
 
+    //tocador de som
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
@@ -29,39 +34,64 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
             tecla.classList.add('ativa');
         }
 
-        if (evento.code === "Numpad1") {
+         if (evento.code === "Numpad1") {
         document.getElementById("btn1").click();
-        listaDeTeclas[0].add('ativa');
+        listaDeTeclas[6].classList.add('ativa');
         } else if (event.code === "Numpad2") {
         document.getElementById("btn2").click();
-        document.getElementById("btn2").add('ativa');
+        listaDeTeclas[7].classList.add('ativa');
         } else if (event.code === "Numpad3") {
         document.getElementById("btn3").click();
-        tecla.classList.add('ativa');
+        listaDeTeclas[8].classList.add('ativa');
         } else if (event.code === "Numpad4") {
-        tecla.classList.add('ativa');
         document.getElementById("btn4").click();
+        listaDeTeclas[3].classList.add('ativa');
         } else if (event.code === "Numpad5") {
-        tecla.classList.add('ativa');
         document.getElementById("btn5").click();
+        listaDeTeclas[4].classList.add('ativa');
         } else if (event.code === "Numpad6") {
-        tecla.classList.add('ativa');
         document.getElementById("btn6").click();
+        listaDeTeclas[5].classList.add('ativa');
         } else if (event.code === "Numpad7") {
-        tecla.classList.add('ativa');
         document.getElementById("btn7").click();
+        listaDeTeclas[0].classList.add('ativa');
         } else if (event.code === "Numpad8") {
-        tecla.classList.add('ativa');
         document.getElementById("btn8").click();
+        listaDeTeclas[1].classList.add('ativa');
         } else if (event.code === "Numpad9") {
-        tecla.classList.add('ativa');
         document.getElementById("btn9").click();
+        listaDeTeclas[2].classList.add('ativa');
+        }
+
+        tecla.onkeyup = function () {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.remove('ativa');
+        }
+        if (evento.code === "Numpad1") {
+        listaDeTeclas[6].classList.remove('ativa');
+        } else if (event.code === "Numpad2") {
+        listaDeTeclas[7].classList.remove('ativa');
+        } else if (event.code === "Numpad3") {
+        listaDeTeclas[8].classList.remove('ativa');
+        } else if (event.code === "Numpad4") {
+        listaDeTeclas[3].classList.remove('ativa');
+        } else if (event.code === "Numpad5") {
+        listaDeTeclas[4].classList.remove('ativa');
+        } else if (event.code === "Numpad6") {
+        listaDeTeclas[5].classList.remove('ativa');
+        } else if (event.code === "Numpad7") {
+        listaDeTeclas[0].classList.remove('ativa');
+        } else if (event.code === "Numpad8") {
+        listaDeTeclas[1].classList.remove('ativa');
+        } else if (event.code === "Numpad9") {
+        listaDeTeclas[2].classList.remove('ativa');
+        }
+
         }
     }
 
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+
 
 }
 
@@ -69,10 +99,4 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
       });
 
-/*
-document.addEventListener('keydown', function(event) {
-  if (event.keyCode >= 48 && event.keyCode <= 57) {
-    // code to press the button
-  }
-});
-*/
+
